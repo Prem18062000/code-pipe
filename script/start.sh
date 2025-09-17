4024af6cd8965e9a0788c718ab590c61617987cd
Ubuntu <ubuntu@ip-172-31-5-107.ap-south-1.compute.internal>
2025-09-17 11:46:55 +0000

    Changes2

--- script/start.sh
+++ script/start.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-
-# Go to the app directory
-cd /home/ubuntu/myapp
-
-# Install dependencies (again, just in case)
-npm install
-
-# Kill existing app running on port 80 (optional, advanced)
-# sudo fuser -k 80/tcp
-
-# Start app in background
-nohup npm start > app.log 2>&1 &
-
+set -euxo pipefail
+cd /home/ubuntu/myapp || exit 1
+# e.g., npm ci > /var/log/myapp/npm-install.log 2>&1
+# start and redirect logs
+nohup node index.js > /var/log/myapp/app.log 2>&1 &
